81766d91a4a2a910943859399c12bd6411be2db9
Deepa Kumari <14.11deepa.kumari@gmail.com>
2024-05-19 01:54:56 +0530

    Add files via upload

---
+++ custom.php
@@ -0,0 +1,31 @@
+<?php
+
+
+$name = $_POST['name'];
+
+$visitor_email = $_POST['email'];
+
+$message = $_POST['message'];
+
+$email_from = 'deepa14112612@gmail.com';
+
+$email_subject = "New Form Submission";
+
+$email_body = "User Name: $name.\n".
+"User Email: $visitor_email.\n".
+" User Message: $message.\n";
+
+$to = "deepa14112612@gmail.com";
+
+$headers = "From: $email_from \r\n";
+
+$headers. "Reply-To: $visitor_email \r\n";
+
+mail($to, $email_subject, $email_body, $headers);
+
+header("Access-Control-Allow-Origin", "*");
+header("Location: index.html");
+
+
+
+?>
